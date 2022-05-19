cd73e9abb35397bf6412775072682d956dd36d79
Stephen Fairchild <sfairchild@maxdigital.com>
2022-05-19 15:03:11 -0400

    upload files to cloudinary

--- api/resonate.js
+++ api/resonate.js
@@ -1,4 +1,46 @@
-export default async function handler(request, response) {
-    console.log("Request received!");
-    return response.status(200).json({ data: {foo: "bar"} });
+require('dotenv').config()
+const multiparty = require("multiparty");
+const cloudinary = require('cloudinary');
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET,
+    secure: true,
+});
+
+module.exports = async (request, response) => {
+    if (request.method === "POST") {
+        const form = new multiparty.Form();
+        const data = await new Promise((resolve, reject) => {
+            form.parse(request, function (err, fields, files) {
+                if (err) reject({ err });
+                if (files.length > 1) {
+                    throw new Error("We currently only support uploading a single file.")
+                }
+
+                resolve({ fields, files });
+            });
+        });
+
+        let photoPath = data.files.photo[0].path
+        const result = await new Promise((resolve, reject) => {
+            cloudinary.v2.uploader.upload(photoPath, function(error, result) {
+                if (error) {
+                    console.log(JSON.stringify(error))
+                    throw new Error("Unable to upload to Cloudinary. Check the logs")
+                }
+
+                resolve(result)
+            });
+        })
+
+        return response.status(200).json({ data: result });
+    }
 }
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
